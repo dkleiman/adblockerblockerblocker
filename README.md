0e14fbceb30b67e1a27c350f11a2a7750fd44681
dkleiman <dave.kleiman2@gmail.com>
2016-05-07 14:46:48 -0400

    Added reference to stackoverflow question

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Logo](https://github.com/dkleiman/adblockerblockerblocker/raw/master/icon128.png)
 
-Small browser extension to block simple sites that block adblocker.
+Small browser extension to block simple sites that block adblocker based on the stackoverflow answer [here](http://stackoverflow.com/questions/10412651/how-can-i-remove-script-elements-before-they-are-being-executed).
 
 Just click on the icon to run a script that reloads the page with none of their javascript.
 
--- content.js
+++ content.js
@@ -1,10 +1,18 @@
+var some = function(list, callback){
+	list.forEach(function(item){
+		if(callback(item)) return true;
+	});
+	return false;
+};
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var xhr = new XMLHttpRequest,
 			    content,
 			    doc,
-			    scripts;
+			    scripts,
+			    whitelist;
 
 			xhr.open( "GET", document.URL, false );
 			xhr.send(null);
@@ -16,12 +24,48 @@ chrome.runtime.onMessage.addListener(
 			doc.write(content);
 			doc.close();
 
+			whitelist = [
+				/dojo/i,
+				/jquery/i,
+				/midori/i,
+				/mootools/i,
+				/prototype/i,
+				/react/i,
+				/meteor/i,
+				/yui/i,
+				/bitsycode/i,
+				/anychart/i,
+				/d3/i,
+				/highcharts/i,
+				/easlejs/i,
+				/moment/i,
+				/createjs/i,
+				/jit/i,
+				/pixi/i,
+				/plotly/i,
+				/processing/i,
+				/raphael/i,
+				/snap/i,
+				/swfobject/i,
+				/three/i,
+				/velocity/i,
+				/whitestorm/i,
+				/angular/i,
+				/underscore/i,
+				/forbes_welcome/i
+			];
+
 
 			scripts = doc.getElementsByTagName("script");
 			//Modify scripts as you please
 			[].forEach.call( scripts, function( script ) {
+				var src = script.getAttribute("src");
+				if(!some(whitelist, function(item){ return item.test(src);})){
 			    script.removeAttribute("src");
-			    script.innerHTML = 'var do = "nothing";';
+			    script.innerHTML = '';
+			  } else {
+			  	console.log("whitelisted: ", src);
+			  }
 			});
 
 			//Doing this will activate all the modified scripts and the "old page" will be gone as the document is replaced
